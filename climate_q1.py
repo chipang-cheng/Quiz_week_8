badbf317b45d1769fd3ea2baa4d68bbbefa55ef8
chipang_cheng <chipang.cheng@griffithuni.edu.au>
2023-09-13 14:32:31 +1000

    Added code to connect to db and store as list

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,22 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
 years = []
 co2 = []
 temp = []
+
+cursor.execute("SELECT year, co2, temperature FROM ClimateData")
+data = cursor.fetchall()
+
+for row in data:
+    year, tco2, temperature = row
+    years.append(year)
+    co2.append(tco2)
+    temp.append(temperature)
+
+conn.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
